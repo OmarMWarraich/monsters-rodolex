f37e4c117e477bb4edd10f5b00e37a33059c5a7c
OmarMWarraich <earthling986@gmail.com>
2024-02-03 17:12:06 +0500

    feat: Users w/ Search Functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
+import { Component } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
+import { toHaveStyle } from '@testing-library/jest-dom/matchers';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      monsters: [],
+      searchField: ''
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }));
+  }
+
+  onSearchChange = (e) => {
+    const searchField = e.target.value.toLocaleLowerCase();
+    this.setState({ searchField });
+  }
+  
+  render() {
+
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+    
+    const filterMonsters = monsters.filter((monster) => {
+          return monster.name.toLowerCase().includes(searchField);
+        }
+        )
+    return (
+      <div className="App">
+        <input className='search' type='search' placeholder='search monsters' onChange={onSearchChange}/>
+        {
+          filterMonsters.map((monster) => (
+            <h1 key={monster.id}>{monster.name}</h1>
+          ))
+        }
+      </div>
+    );
+  }
 }
-
 export default App;
